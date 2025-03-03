655583f90fe6eda0e135fc20beea42f10a8645fa
Quatov <128380652+Bowqua@users.noreply.github.com>
2025-03-03 21:55:01 +0500

    Add files via upload

---
+++ Part_one.cs
@@ -0,0 +1,56 @@
+﻿// public abstract class PartOne
+// {
+//     public static void Write()
+//     {
+//         Console.OutputEncoding = System.Text.Encoding.UTF8;
+//         Console.Write("Привет, мир!");
+//     }
+// }
+
+// namespace lecture_C_;
+//
+// public class MainClass
+// {
+//     public static void Main(string[] args)
+//     {
+//         string message = ReadInput();
+//         string result = message;
+//
+//         Console.WriteLine(result);
+//     }
+//
+//     public static string ReadInput()
+//     {
+//         return Console.ReadLine();
+//     }
+// }
+
+// public class MainClass
+// {
+//     public static void Main()
+//     {
+//         var inputMessages = ReadInput();
+//         Console.WriteLine(inputMessages[0] + " " + inputMessages[1]);
+//     }
+//
+//     private static string[] ReadInput()
+//     {
+//         return Console.ReadLine().Split(" | ");
+//     }
+// }
+
+// public class MainClass
+// {
+//     private static void Main()
+//     {
+//         var inputValues = ReadInput();
+//         Console.WriteLine(inputValues[0] + " " + int.Parse(inputValues[1]) * 2);
+//     }
+//
+//     private static string[] ReadInput()
+//     {
+//         return Console.ReadLine()!.Split(" | ");
+//     }
+// }
+
+
